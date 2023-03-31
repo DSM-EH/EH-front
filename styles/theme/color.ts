1f30b6576c2f12922482138584ddf86eb06b7fec
kimkh05 <nestjs01@naver.com>
2023-03-31 09:25:10 +0900

    feat: theme color & font

--- styles/theme/color.ts
+++ styles/theme/color.ts
@@ -1,3 +1,26 @@
-export const color = {} as const;
+export const color = {
+  main01: "#FFACAC",
+  main02: "#FFBFA9",
+  main03: "#FFEBB4",
+  main04: "#FBFFB1",
+  main05: "#FFFCF0",
+  gray100: "#FDFDFD",
+  gray200: "#EDEDED",
+  gray300: "#D3D3D3",
+  gray400: "#BDBDBD",
+  gray500: "#A7A7A7",
+  gray600: "#929292",
+  gray700: "#7C7C7C",
+  gray800: "#505050",
+  gray900: "#343434",
+  gray000: "#141414",
+  background: "#FFFBFB",
+  error: "#DC3035",
+  check: "#04DF00",
+  focus: "#006EFF",
+  white: "#FFFFFF",
+  black: "#000000",
+
+} as const;
 
 export type keyOfColorType = keyof typeof color;
