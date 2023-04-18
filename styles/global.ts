8e3072938889b42235e7e0971fe4a4bde6aae2a4
kimkh05 <nestjs01@naver.com>
2023-04-18 17:03:54 +0900

    feat: font-family

--- styles/global.ts
+++ styles/global.ts
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 
 const global = css`
+  @import url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css');
   * {
     margin: 0;
     padding: 0;
@@ -8,6 +9,7 @@ const global = css`
     box-sizing: border-box;
     border: 0;
     text-decoration: none;
+    font-family: pretendard, sans-serif;
   }
 `;
 
