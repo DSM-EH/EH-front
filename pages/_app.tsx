00eb0e688a0cc50751fe3d2eda4b9539e3e84698
kimkh05 <nestjs01@naver.com>
2023-04-18 20:00:36 +0900

    feat: react-query & recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
 import type { AppProps } from 'next/app';
 import { StyleProvider } from '@/styles';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+      staleTime: 5000,
+    },
+  },
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <StyleProvider>
-      <Component {...pageProps} />
-    </StyleProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <StyleProvider>
+          <Component {...pageProps} />
+        </StyleProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
 
