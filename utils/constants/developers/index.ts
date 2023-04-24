9779c4e630d1faf5eb5d2287b1491af429486b79
kimkh05 <nestjs01@naver.com>
2023-04-24 13:43:23 +0900

    feat: developer data

---
+++ utils/constants/developers/index.ts
@@ -0,0 +1,23 @@
+export interface DeveloperType {
+  id: number;
+  name: string;
+  github: string;
+}
+
+export const Developers: DeveloperType[] = [
+  {
+    id: 1,
+    name: '김경호',
+    github: 'kimkh05',
+  },
+  {
+    id: 2,
+    name: '이준범',
+    github: 'Zi-nal',
+  },
+  {
+    id: 3,
+    name: '이준서',
+    github: 'glay415',
+  },
+];
