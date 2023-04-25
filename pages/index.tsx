1c78e88f24d32675051bb490aa7f77ae7185fc74
kimkh05 <nestjs01@naver.com>
2023-04-25 10:28:42 +0900

    feat: main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,39 @@
+import Footer from '@/components/common/footer';
 import Head from 'next/head';
+import Header from '@/components/common/header';
+import ImageContainer from '@/components/main/ImageContainer';
+import IntroduceContainer from '@/components/main/Introduce';
+import { Main01, Main02 } from '@/assets';
+import styled from '@emotion/styled';
 
 const Home = () => {
   return (
-    <>
-      <Head>Head</Head>
-      <span>Test</span>
-    </>
+    <_Wrapper>
+      <Head>
+        <title>EH</title>
+      </Head>
+      <Header />
+      <ImageContainer />
+      <IntroduceContainer
+        imageUrl={Main01}
+        text={['EH에서 운동과 식단을 공유할 수 있어요.', '소통을 통해 원하는 정보를 얻어보세요 !']}
+        reverse={false}
+      />
+      <IntroduceContainer
+        imageUrl={Main02}
+        text={['그룹을 참가하여 같이 운동할 사람을 찾을 수 있어요! ', '그룹과 함께 배우며 운동합시다 !']}
+        reverse={true}
+      />
+      <Footer />
+    </_Wrapper>
   );
 };
 
 export default Home;
+
+const _Wrapper = styled.div`
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
