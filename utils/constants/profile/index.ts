a7d697f09e50245b6ac6b73a2bb3cb8bf59f9adf
kimkh05 <nestjs01@naver.com>
2023-05-01 14:40:27 +0900

    feat: dummy

---
+++ utils/constants/profile/index.ts
@@ -0,0 +1,16 @@
+import { Main01 } from '@/assets';
+import { ProfileType } from '@/types/profile';
+
+export const mypage: ProfileType = {
+  imageUrl: Main01,
+  name: '차은우',
+  introduce: '안녕하세요 차은우입니다.',
+  myprofile: true,
+};
+
+export const otherProfile: ProfileType = {
+  imageUrl: Main01,
+  name: '이준범',
+  introduce: '안녕하세요 이준범입니다.',
+  myprofile: false,
+};
