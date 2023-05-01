816038270f2e0ea973cd9e0eb90c7300ce4bfb7e
kimkh05 <nestjs01@naver.com>
2023-05-01 11:58:13 +0900

    feat: find image

---
+++ components/common/find/FindImage.tsx
@@ -0,0 +1,76 @@
+import styled from '@emotion/styled';
+import { FindImageLogo } from '@/assets';
+import Image from 'next/image';
+import { ChangeEvent, useState, useMemo, useRef, RefObject } from 'react';
+
+interface UploadType {
+  file: File;
+  thumbnail: string;
+  type: string;
+}
+
+interface PropsType {
+  name: 'imageUrl' | 'groupBackgroundImageUrl' | 'posterImageUrl';
+  height?: number;
+}
+
+const FindImage = ({ name, height }: PropsType) => {
+  const [image, setImage] = useState<UploadType>();
+  const imageRef: RefObject<HTMLInputElement> = useRef<HTMLInputElement>(null);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const fileList: FileList | null = e.target.files;
+    if (fileList && fileList[0]) {
+      const url: string = URL.createObjectURL(fileList[0]);
+      setImage({
+        file: fileList[0],
+        thumbnail: url,
+        type: fileList[0].type.slice(0, 5),
+      });
+    }
+  };
+
+  const showImage = useMemo(() => {
+    if (!image && image === undefined) {
+      return (
+        <>
+          <Image src={FindImageLogo} alt="FindImage" />
+          <_Text>이미지를 선택하세요</_Text>
+        </>
+      );
+    }
+    return <Image src={image?.thumbnail} alt={image?.type} />;
+  }, [image]);
+
+  return (
+    <_Wrapper height={height} onClick={() => imageRef.current?.click()}>
+      {showImage}
+      <_FileSelector ref={imageRef} type="file" accept="image/*" onChange={onChange} />
+    </_Wrapper>
+  );
+};
+
+export default FindImage;
+
+const _Wrapper = styled.form<{ height?: number }>`
+  width: 340px;
+  height: ${({ height = 200 }) => `${height}px`};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 2px dashed ${({ theme }) => theme.color.gray300};
+  cursor: pointer;
+`;
+
+const _FileSelector = styled.input`
+  width: 340px;
+  height: 200px;
+  display: none;
+`;
+
+const _Text = styled.p`
+  margin-top: 10px;
+  color: ${({ theme }) => theme.color.gray300};
+  ${({ theme }) => theme.font.body5};
+`;
