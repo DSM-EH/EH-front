76c7f191de14ead01e254b0a6994f80c7e0f1628
kimkh05 <nestjs01@naver.com>
2023-05-01 11:55:08 +0900

    feat: init image

--- assets/index.ts
+++ assets/index.ts
@@ -4,6 +4,7 @@ export { default as Clue } from './image/clue.svg';
 export { default as GithubLogo } from './image/github.svg';
 export { default as Main01 } from './image/main01.svg';
 export { default as Main02 } from './image/main02.svg';
+export { default as FindImageLogo } from './image/image.svg';
 
 // png
 export { default as Introduce } from './image/introduce.png';
