b9b7f5c4454158cc2748bff9f1cd634ee204669a
kimkh05 <nestjs01@naver.com>
2023-05-01 14:38:05 +0900

    style: prettier

--- types/group/index.ts
+++ types/group/index.ts
@@ -1,12 +1,12 @@
-import { StaticImageData } from "next/image";
+import { StaticImageData } from 'next/image';
 
-export interface CreateGroupType{
-    title: string;
-    imageUrl: string | StaticImageData;
-    introduce: string;
-    groupBackgroundImageUrl: string | StaticImageData;
-    contents: string;
-    posterImageUrl: string | StaticImageData;
-    member: number;
-    time: string;
+export interface CreateGroupType {
+  title: string;
+  imageUrl: string | StaticImageData;
+  introduce: string;
+  groupBackgroundImageUrl: string | StaticImageData;
+  contents: string;
+  posterImageUrl: string | StaticImageData;
+  member: number;
+  time: string;
 }
