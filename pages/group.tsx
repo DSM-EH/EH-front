d4b356a251aed9fbb6632e73e9d783dd55734f13
kimkh05 <nestjs01@naver.com>
2023-05-01 12:04:56 +0900

    fix: use wrapper component

--- pages/group.tsx
+++ pages/group.tsx
@@ -1,22 +1,18 @@
 import Header from '@/components/common/header';
-import styled from '@emotion/styled';
 import Head from 'next/head';
 import GroupPart from '@/components/group/GroupPart';
+import Wrapper from '@/components/common/wrapper';
 
 const GroupPage = () => {
   return (
-    <_Wrapper>
+    <Wrapper>
       <Head>
         <title>EH 운동 그룹</title>
       </Head>
       <Header />
       <GroupPart />
-    </_Wrapper>
+    </Wrapper>
   );
 };
 
 export default GroupPage;
-
-const _Wrapper = styled.div`
-  width: 100vw;
-`;
