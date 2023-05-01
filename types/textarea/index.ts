a5600127b0acb66f5f19ddb4ca87576c7fd67605
kimkh05 <nestjs01@naver.com>
2023-05-01 11:56:18 +0900

    feat: textarea type

---
+++ types/textarea/index.ts
@@ -0,0 +1,9 @@
+import { ChangeEvent } from "react";
+
+export interface TextareaType {
+  name: string;
+  value: string;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
+  label: string;
+  placeholder: string;
+}
