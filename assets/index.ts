612aa13c9592f8a75500d172dcbb3637425130ca
kimkh05 <nestjs01@naver.com>
2023-05-12 12:33:03 +0900

    feat: init google logo

--- assets/index.ts
+++ assets/index.ts
@@ -5,6 +5,7 @@ export { default as GithubLogo } from './image/github.svg';
 export { default as Main01 } from './image/main01.svg';
 export { default as Main02 } from './image/main02.svg';
 export { default as FindImageLogo } from './image/image.svg';
+export { default as GoogleLogo } from './image/google.svg';
 
 // png
 export { default as Introduce } from './image/introduce.png';
