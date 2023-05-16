33585c9ceaa55ea1ecf0742731aff8a9638ac843
kimkh05 <nestjs01@naver.com>
2023-05-16 20:27:14 +0900

    fix: init animations

--- components/common/modal/ModalItem.tsx
+++ components/common/modal/ModalItem.tsx
@@ -1,14 +1,17 @@
+import transitions from '@/lib/styles/transitions';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { ReactNode, HTMLAttributes } from 'react';
 
 interface PropsType extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   height?: number;
+  modal: boolean;
 }
 
-const ModalItem = ({ children, height, ...rest }: PropsType) => {
+const ModalItem = ({ children, height, modal, ...rest }: PropsType) => {
   return (
-    <_Wrapper {...rest} height={height}>
+    <_Wrapper {...rest} modal={modal} height={height}>
       {children}
     </_Wrapper>
   );
@@ -16,7 +19,7 @@ const ModalItem = ({ children, height, ...rest }: PropsType) => {
 
 export default ModalItem;
 
-const _Wrapper = styled.div<{ height?: number }>`
+const _Wrapper = styled.div<{ height?: number; modal: boolean }>`
   width: 650px;
   height: ${({ height = 320 }) => height}px;
   background-color: ${({ theme }) => theme.color.background};
@@ -26,4 +29,12 @@ const _Wrapper = styled.div<{ height?: number }>`
   align-items: center;
   padding: 100px 0;
   justify-content: space-between;
+  ${props =>
+    props.modal
+      ? css`
+          animation: ${transitions.popInFromBottom} 0.4s forwards ease-in-out;
+        `
+      : css`
+          animation: ${transitions.popOutToBottom} 0.2s forwards ease-in-out;
+        `}
 `;
