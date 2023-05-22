05f34506682f2cadb828f1f28b12191d031b21ee
kimkh05 <nestjs01@naver.com>
2023-05-22 12:23:39 +0900

    feat: member

---
+++ utils/constants/member/index.ts
@@ -0,0 +1,22 @@
+export const members = [
+  {
+    id: 1,
+    name: '김민수',
+    profile: '',
+  },
+  {
+    id: 2,
+    name: '이준범',
+    profile: '',
+  },
+  {
+    id: 3,
+    name: '이현주',
+    profile: '',
+  },
+  {
+    id: 4,
+    name: '정현수',
+    profile: '',
+  },
+];
