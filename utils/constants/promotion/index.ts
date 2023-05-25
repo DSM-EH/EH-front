78cb099117f2cfb20f04ac32715a2f233938386f
kimkh05 <nestjs01@naver.com>
2023-05-25 13:52:24 +0900

    fix: dummy

--- utils/constants/promotion/index.ts
+++ utils/constants/promotion/index.ts
@@ -1,7 +1,7 @@
 export const promotion = {
-    title: 'Promotion',
-    contents: '안녕 친구들 안녕 친구들',
-    recruitmentMember: 4,
-    gatheringTime: "목요일 오전 9시",
-    imageUrl: ''
-}
+  title: 'Promotion',
+  contents: '안녕 친구들 안녕 친구들',
+  recruitmentMember: 4,
+  gatheringTime: '목요일 오전 9시',
+  imageUrl: '',
+};
