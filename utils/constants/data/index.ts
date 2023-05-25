8dd13481041fe9db59f58c10a17dbbf973afe66e
kimkh05 <nestjs01@naver.com>
2023-05-25 13:52:13 +0900

    feat: dummy

---
+++ utils/constants/data/index.ts
@@ -0,0 +1,66 @@
+export const communication = [
+  {
+    id: 0,
+    title: 'Communication',
+    contents: '안녕 친구들 안녕 친구들',
+    profileImageUrl: '',
+    date: '2021-09-01',
+    name: '안녕',
+    imageUrl: '',
+    comment: [
+      {
+        profileImageUrl: '',
+        name: '김철수',
+        contents: '안녕하세요',
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: 'Communication',
+    contents: '안녕 친구들 안녕 친구들',
+    profileImageUrl: '',
+    date: '2021-09-01',
+    name: '',
+    imageUrl: '',
+    comment: [
+      {
+        profileImageUrl: '',
+        name: '김철수',
+        contents: '안녕하세요',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Communication',
+    contents: '안녕 친구들 안녕 친구들',
+    profileImageUrl: '',
+    date: '2021-09-01',
+    name: '',
+    imageUrl: '',
+    comment: [
+      {
+        profileImageUrl: '',
+        name: '김철수',
+        contents: '안녕하세요',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Communication',
+    contents: '안녕 친구들 안녕 친구들',
+    profileImageUrl: '',
+    date: '2021-09-01',
+    name: '',
+    imageUrl: '',
+    comment: [
+      {
+        profileImageUrl: '',
+        name: '김철수',
+        contents: '안녕하세요',
+      },
+    ],
+  },
+];
