2fb129ea4e7e31f15c26fb68748818e6f8793aba
kimkh05 <nestjs01@naver.com>
2023-05-30 23:25:32 +0900

    feat: setting instance

---
+++ apis/axios.ts
@@ -0,0 +1,19 @@
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
+import { getCookie } from '@/utils/cookie/cookie';
+
+const instance = axios.create({
+  baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  timeout: 2000,
+});
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    config.headers.Authorization = `Bearer ${getCookie('accessToken')}`;
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
+
+export default instance;
