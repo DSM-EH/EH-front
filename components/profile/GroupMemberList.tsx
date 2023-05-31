5224b1b44618efeb3739c9b0a869ac91e638efba
kimkh05 <nestjs01@naver.com>
2023-05-31 12:22:28 +0900

    feat: get member

--- components/profile/GroupMemberList.tsx
+++ components/profile/GroupMemberList.tsx
@@ -3,20 +3,52 @@ import { Expansion } from '@/assets';
 import Image from 'next/image';
 import { members } from '@/utils/constants/member';
 import GroupMemberListItem from './GroupMemberListItem';
+import { useEffect, useState } from 'react';
+import { getMembers } from '@/apis/getMembers';
+import { customToast } from '@/utils/toast/toast';
 
 interface PropsType {
   title: string;
   isLoading: boolean;
+  onClick?: () => void;
 }
 
-const GroupMemberList = ({ title, isLoading }: PropsType) => {
+interface MemberType {
+  id: number;
+  email: string;
+  password: string;
+  nickname: string;
+  description: string;
+  profile_image_url: string;
+}
+
+const GroupMemberList = ({ title, isLoading, onClick }: PropsType) => {
+  const [member, setMember] = useState<MemberType[]>([]);
+
+  useEffect(() => {
+    const id: string | null = localStorage.getItem('groupId');
+    if (!id) {
+      customToast('잘못된 접근입니다.', 'error');
+      return;
+    }
+    getMembers(id)
+      .then(res => {
+        const { data } = res;
+        setMember(data);
+      })
+      .catch((err: unknown) => {
+        console.error(err);
+        customToast('개발자 에러', 'error');
+      });
+  }, []);
+
   return (
     <_Wrapper>
       <_UpperWrapper>
         <_Title>{title}</_Title>
-        <_VectorImage src={Expansion} alt="vector" />
+        <_VectorImage src={Expansion} onClick={onClick} alt="vector" />
       </_UpperWrapper>
-      {members.map(element => (
+      {member.map(element => (
         <GroupMemberListItem isLoading={isLoading} key={element.id} {...element} />
       ))}
     </_Wrapper>
