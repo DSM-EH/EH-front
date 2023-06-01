d8f428f099c7e28dc979a14deaf39a63b2bfb28e
kimkh05 <nestjs01@naver.com>
2023-06-01 14:10:05 +0900

    feat: type

--- types/profile/index.ts
+++ types/profile/index.ts
@@ -1,8 +1,17 @@
-import { StaticImageData } from "next/image";
+import { StaticImageData } from 'next/image';
 
-export interface ProfileType{
-    imageUrl: string | StaticImageData;
-    name: string;
-    introduce: string;
-    myProfile: boolean;
+export interface ProfileType {
+  imageUrl: string | StaticImageData;
+  name: string;
+  introduce: string;
+  myProfile: boolean;
 }
+export interface ProfileApiType {
+  id: number;
+  email: string;
+  password: string;
+  nickname: string;
+  description: string;
+  profile_image_url: string;
+}
+
---
+++ types/request/index.ts
@@ -0,0 +1,5 @@
+export interface RequestType {
+  email: string
+  group_id: string 
+  intro: string
+}
