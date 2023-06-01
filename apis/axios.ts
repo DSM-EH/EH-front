d1029d51ebd50a94a0569759f1e53067543dc29f
kimkh05 <nestjs01@naver.com>
2023-06-01 14:18:33 +0900

    fix: remove unnecessary code

--- apis/axios.ts
+++ apis/axios.ts
@@ -1,19 +1,8 @@
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import { getCookie } from '@/utils/cookie/cookie';
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   timeout: 2000,
 });
-
-// instance.interceptors.request.use(
-//   (config: InternalAxiosRequestConfig) => {
-//     config.headers.Authorization = `Bearer ${getCookie('accessToken')}`;
-//     return config;
-//   },
-//   (error: AxiosError) => {
-//     return Promise.reject(error);
-//   },
-// );
 
 export default instance;
