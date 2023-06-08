d56152ed70dc889769e3057b964134120170fdf4
kimkh05 <nestjs01@naver.com>
2023-06-08 19:45:04 +0900

    feat: useEffect api using

--- components/profile/GroupList.tsx
+++ components/profile/GroupList.tsx
@@ -1,14 +1,38 @@
 import styled from '@emotion/styled';
 import Wrapper from '../common/wrapper';
 import { GroupItemType, GroupItemData } from '@/utils/constants/group';
+import { getMyGroup } from '@/apis/getMyGroup';
 import ProfileGroupItem from './GroupItem';
+import { useEffect, useState } from 'react';
+
+interface GroupType {
+  id: number;
+  title: string;
+  profile_image: string;
+  background_image: string;
+  description: string;
+  poster_image: string;
+  max_people: number;
+  set_time: string;
+}
 
 const ProfileGroupList = () => {
+  const [groupItems, setGroupItems] = useState<GroupType[]>([]);
+
+  useEffect(() => {
+    getMyGroup().then(res=> {
+      console.log(res.data);
+      setGroupItems(res.data);
+    }).catch((err: unknown) => {
+      console.error(err);
+    })
+  }, [])
+
   return (
     <_Wrapper>
       <_Title>참여한 그룹</_Title>
       <_GroupListWrapper>
-        {GroupItemData.map((element: GroupItemType) => (
+        {groupItems.map((element: GroupType) => (
           <ProfileGroupItem key={element.id} {...element} />
         ))}
       </_GroupListWrapper>
