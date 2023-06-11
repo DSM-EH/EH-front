b4760f2a8eddb724728a92450015cb1d2d3fb02e
kimkh05 <nestjs01@naver.com>
2023-06-11 23:08:08 +0900

    fix: title  props

--- pages/group/[groupName]/member.tsx
+++ pages/group/[groupName]/member.tsx
@@ -1,17 +1,20 @@
 import Header from '@/components/common/header';
 import Wrapper from '@/components/common/wrapper';
 import Head from 'next/head';
-import router from 'next/router';
 import GroupMemberList from '@/components/group/GroupMemberList';
+import { NextRouter, useRouter } from 'next/router';
 
 const MemberPage = () => {
+  const router: NextRouter = useRouter();
+  const { groupName } = router.query as { groupName: string };
+
   return (
     <Wrapper>
       <Head>
         <title>그룹멤버들</title>
       </Head>
       <Header />
-      <GroupMemberList title={'group'} />
+      <GroupMemberList title={groupName} />
     </Wrapper>
   );
 };
