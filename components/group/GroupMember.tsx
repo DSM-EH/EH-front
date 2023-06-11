1062677a113d756e0e900f70844a9821c9ddf12d
kimkh05 <nestjs01@naver.com>
2023-06-11 23:07:51 +0900

    feat: group member item

--- components/group/GroupMember.tsx
+++ components/group/GroupMember.tsx
@@ -1,21 +1,43 @@
 import styled from '@emotion/styled';
-import Image, { StaticImageData } from 'next/image';
 
-interface PropsType {
-  name: string;
-  profileImageUrl: string | StaticImageData;
-  createAt: string;
-  onClick: (e?: MouseEvent) => void;
+interface MemberInformationType {
+  id: number;
+  email: string;
+  password: string;
+  nickname: string;
+  description: string;
+  profile_image_url: string;
+  onClick: (information: any) => void;
 }
 
-const GroupMember = ({ name, profileImageUrl, createAt, onClick }: PropsType) => {
+const GroupMember = (props: MemberInformationType) => {
+  const { id, email, password, nickname, description, profile_image_url, onClick }: MemberInformationType = props;
   return (
-    <_Wrapper onClick={() => onClick()}>
-      <span>Test</span>
+    <_Wrapper onClick={() => onClick(props)}>
+      <_ProfileImage src={profile_image_url} alt={nickname} />
+      <_NameText>{nickname}</_NameText>
     </_Wrapper>
   );
 };
 
 export default GroupMember;
 
-const _Wrapper = styled.div``;
+const _Wrapper = styled.div`
+  width: 250px;
+  height: max-content;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const _ProfileImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 5px;
+  margin-right: 40px;
+`;
+
+const _NameText = styled.span`
+  ${({ theme }) => theme.font.body3};
+  color: ${({ theme }) => theme.color.black};
+`;
